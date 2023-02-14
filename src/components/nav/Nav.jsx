962a553d4f3d5aca739a399745236b871f5f4024
wanjiku <annritawanjiku77@gmail.com>
2023-02-14 08:35:19 +0300

    added nav links and icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './nav.css'
+import {GoHome} from 'react-icons/go'
+import {RiComputerLine, RiServiceLine} from 'react-icons/ri'
+import {BiUser, BiMessageRoundedDetail} from 'react-icons/bi'
 
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><GoHome/></a>
+      <a href="#about"><BiUser/></a>
+      <a href="#experience"><RiComputerLine/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><BiMessageRoundedDetail/></a>
+    </nav>
   )
 }
 
