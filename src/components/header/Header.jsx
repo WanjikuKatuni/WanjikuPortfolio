754fdb5767905c4df544d6c826cbc9e44f410b9f
wanjiku <annritawanjiku77@gmail.com>
2023-02-14 07:57:26 +0300

    added header details to portfolio

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './header.css'
-
+import CTA from './CTA'
+import Headersocials from './Headersocials'
+import meimg from '../../assets/wanjiku.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5> Hello I'm</h5>
+        <h1> Ann Wanjiku</h1>
+        <h5 className="text-light"> Fullstack Developer </h5>
+        <CTA/>
+        <Headersocials/>
+
+
+        <div className="me">
+          <img src={meimg} alt='me'/>
+        </div>
+        <a href="#contact" className='scroll__down'> ScrollDown</a>
+      </div>
+
+    </header>
   )
 }
 
